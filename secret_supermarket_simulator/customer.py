c7c4606b86037aac4b4f5ccd2dbc07595753bd2f
Maryam Beygmohammadi <Maryam@MacBook-Pro-von-Marc.local>
2023-03-02 16:56:54 +0100

    the classes and simulator for one day

---
+++ secret_supermarket_simulator/customer.py
@@ -0,0 +1,38 @@
+
+import numpy as np
+import pandas as pd
+
+matrix_in = pd.read_csv('../data/transition_matrix.csv')
+matrix_in.info()
+matrix_in.set_index('location',inplace= True)
+
+
+
+class Customer: 
+    PROBABILITIES= matrix_in.copy()
+    def __init__(self,id)-> None:
+        """_summary_
+        Args:
+            id (_type_): _description_
+            state (_type_): _description_
+        """
+
+        self.id = id
+        self.location = 'entrance' 
+        self.active = True
+
+    def __repr__(self) -> str:
+        
+        return f"Customer {self.id} is at {self.location}."
+
+    def next_location(self):
+        if self.active:
+            locations = Customer.PROBABILITIES.columns
+            self.location = np.random.choice(a=locations, p=Customer.PROBABILITIES.loc[self.location])
+            self.active = (self.location != 'checkout')
+        return None
+    
+
+   
+
+
---
+++ secret_supermarket_simulator/supermarket_start.py
@@ -0,0 +1,65 @@
+from customer import Customer
+
+class Supermarket:
+    """manages multiple Customer instances that are currently in the market.
+    """
+
+    def __init__(self, name):        
+        # a list of Customer objects
+        self.customers = []
+        self.minutes = 0
+        self.last_id = 0
+        self.name = name
+
+    def __repr__(self):
+        return 'The current time is supermarket '
+
+    def is_open(self):
+        return self.get_time() != "22:00"
+
+    def get_time(self):
+        """current time in HH:MM format,
+        """
+        hour = 7 + self.minutes // 60
+        minutes = self.minutes % 60
+        timestamp = f"{hour:02d}:{minutes:02d}" 
+        return timestamp
+
+    def print_customers(self):
+        """print all customers with the current time, id, location in CSV format.
+        """
+        printlist = []
+        for customer in self.customers:
+            cus_string = f"The customer {customer.id} is at {customer.location} at {self.get_time()}."
+            print(cus_string)
+            printlist.append(cus_string)
+
+        return None
+
+    def next_minute(self):
+        """propagates all customers to the next state.
+        """
+        self.minutes = self.minutes + 1
+        for customer in self.customers:
+            customer.next_location()
+        return None
+    
+    def add_new_customers(self):
+        '''Adds new customer for every minute. 
+        TODO: using poisson distribiution instead of always one new
+        '''
+        self.last_id += 1
+        self.customers.append(Customer(self.last_id))
+
+        return None
+
+    def remove_exitsting_customers(self):
+        """removes every customer that is not active any more.
+        """
+        for customer in self.customers:
+            if not customer.active:
+                self.customers.remove(customer)
+        return None
+
+
+
